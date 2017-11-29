5d24d3cf6322769346d4e37d2752333d0efcdcd7
Dmitry Shestakov <altais@ukr.net>
2017-11-29 17:47:44 +0200

    fix. Include guard for type_traits_ex.h

--- utils/type_traits_ex.h
+++ utils/type_traits_ex.h
@@ -19,6 +19,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+#ifndef TYPE_TRAITS_EX_H
+#define TYPE_TRAITS_EX_H
 
 #include <type_traits>
 #include <stdint.h>
@@ -97,3 +99,4 @@ namespace Utils {
   };
 }
 
+#endif //TYPE_TRAITS_H
